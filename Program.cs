51103e0c06b70f66d348d100e18bcd13130b3e89
Amaliebra <jba082024@gmail.com>
2024-10-14 10:13:26 +0200

    practice

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _14._10._2024_Csharp_recap;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double todayDate
+        string yoWorld = "Yo World";
+        Console.WriteLine($"{yoWorld}");
+    }
+}
